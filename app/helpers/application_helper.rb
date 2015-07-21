95bea1143be883973036267db954799ce427cd7c
George Borg <georgeborg00@hotmail.com>
2015-07-21 21:49:56 +0100

    overlay autoply on click

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def embed(url)
     youtube_id = url.split("=").last
-    content_tag(:iframe, nil, src: "http://www.youtube-nocookie.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;autoplay=0 frameborder=0 allowfullscreen")
+    content_tag(:iframe, nil, class: "video", src: "http://www.youtube-nocookie.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp; autoplay=0 frameborder=0 allowfullscreen")
    end
    
 end
