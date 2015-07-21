5a79029cf136093c71bbbc3945252022bbfe3d3a
George Borg <georgeborg00@hotmail.com>
2015-07-21 19:45:37 +0100

    scaffold categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721183521) do
+ActiveRecord::Schema.define(version: 20150721184256) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "source"
