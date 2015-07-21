90e86583a6d4f5df715e2b98614cc9bc32af786f
George Borg <georgeborg00@hotmail.com>
2015-07-21 20:37:32 +0100

    CSS

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	has_many :post_categories
-	has_many :posts, through: :post_categories
+	has_many :post_categories, dependent: :destroy
+	has_many :categories, through: :post_categories
 end
--- db/migrate/20150721185327_create_posts_categories.rb
+++ db/migrate/20150721185327_create_posts_categories.rb
@@ -1,7 +1,7 @@
 class CreatePostsCategories < ActiveRecord::Migration
   def change
     create_table :posts_categories do |t|
-      t.integer :category
+      t.integer :category_id
       t.integer :post_id
 
       t.timestamps null: false
