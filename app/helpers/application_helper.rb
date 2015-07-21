e089fe7e3e1210774831d61777493843a0d66dbe
Jack Clackett <jclackett@gmail.com>
2015-07-21 21:10:26 +0100

    post index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def embed(url)
+    youtube_id = url.split("=").last
+    content_tag(:iframe, nil, src: "http://www.youtube-nocookie.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;autoplay=0 frameborder=0 allowfullscreen")
+   end
+   
 end
+
