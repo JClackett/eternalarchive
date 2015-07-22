faae762b4274eee8901e94fed69b0cacb3b0d073
Dan <dlec2@hotmail.co.uk>
2015-07-22 20:14:29 +0100

    changing css, and form text fields

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, notice: 'Your post has been sent to the moderators, it will be displayed subject to approval' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
