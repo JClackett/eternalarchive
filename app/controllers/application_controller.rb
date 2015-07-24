5ff7bdfd853b5cda9d7a306150886504b14cb0d9
George Borg <georgeborg00@hotmail.com>
2015-07-24 16:28:04 +0100

    fixed no category bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,37 @@ class ApplicationController < ActionController::Base
 
   def parents
     @category_healthy = Category.where(categories: { parent: "healthy" } )
-    @category_healthy_name = []
-    @category_healthy.each do |category|
-      @category_healthy_name.push(category.name)
+    if @category_healthy.present?
+	    @category_healthy_name = []
+	    @category_healthy.each do |category|
+	      @category_healthy_name.push(category.name)
+	  	end
+	else
+	  @category_healthy_name = "none"
     end
 
     @category_wealthy = Category.where(categories: { parent: "wealthy" } )
-    @category_wealthy_name = []
-    @category_wealthy.each do |category|
-      @category_wealthy_name.push(category.name)
-    end
+    
+    if @category_wealthy.present?
+	    @category_wealthy_name = []
+	    @category_wealthy.each do |category|
+	      @category_wealthy_name.push(category.name)
+	    end
+	else
+		@category_wealthy_name = "none"
+	end
 
     @category_wise = Category.where(categories: { parent: "wise" } )
-    @category_wise_name = []
-    @category_wise.each do |category|
-      @category_wise_name.push(category.name)
-    end
+    
+    if @category_wise.present?
+    	@category_wise_name = []
+    	@category_wise.each do |category|
+      		@category_wise_name.push(category.name)
+    	end
+    else
+    	@category_wise_name = "none"
+  	end
   end
+
 
 end
