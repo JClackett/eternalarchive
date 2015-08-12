553ad1c90c123c120561476bd7dbaef29c8dd781
Jack Clackett <jclackett@gmail.com>
2015-08-12 21:50:50 +0100

    main header css, changing new post form, adding seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category_list = [
+  [ "Fitness", "healthy" ],
+  [ "Nutrition", "healthy" ],
+  [ "Business", "wealthy" ],
+  [ "Investing", "wealthy" ],
+  [ "Philosophy", "wise" ],
+  [ "Science", "wise" ],
+  [ "Culture", "wise" ],
+  [ "Sociology", "wise" ],
+  [ "Politics", "wise" ],
+]
+
+category_list.each do |name, parent|
+  Category.create( name: name, parent: parent )
+end
