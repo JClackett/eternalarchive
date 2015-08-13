d9c919f98efa0f7dbb2cbb155973b02afb54c972
Jack Clackett <jclackett@gmail.com>
2015-08-13 17:41:15 +0100

    fixing double video problem on soupbowl and css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def embed(url)
     youtube_id = url.split("=").last
-    content_tag(:iframe, nil, class: "video", src: "http://www.youtube-nocookie.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 allowfullscreen autoplay=0")
+    content_tag(:iframe, nil, class: "video", src: "http://www.youtube-nocookie.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0&amp;allowfullscreen=true&amp;autoplay=0")
   end
    
 end
