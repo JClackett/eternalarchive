05062a96a33b7e0a64b77195ff82f2a3d4fd821d
Jack Clackett <jclackett@gmail.com>
2015-08-21 16:49:17 +0100

    fixed fullscreen problems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def embed(url)
     youtube_id = url.split("=").last
 
-    content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 allowfullscreen autoplay=0")
+    content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 autoplay=0" , allowfullscreen:0)
   end
    
 end
