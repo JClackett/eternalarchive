1b781819e9ff341851f7ce13c57d960e7ca02f21
George Borg <georgeborg00@hotmail.com>
2015-08-22 18:11:40 +0100

    absolute mess

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,18 @@ class PostsController < ApplicationController
   def index
     if params[:name].present?
 
-      @posts = Post.joins(:categories).where(categories: { name: params[:name] } ).uniq.paginate(:page => params[:page], :per_page => 6).reverse_order
-
-    else
-      @posts = Post.paginate(:page => params[:page], :per_page =>6).reverse_order
+      @posts = Post.joins(:categories).where(categories: { name: params[:name] } ).uniq.paginate(:page => params[:page], :per_page => 4).reverse_order
       respond_to do |format|
-      format.html # index.html.erb
-      format.js
-      end
+        format.html
+        format.js
+      end 
+    else
+      @posts = Post.paginate(:page => params[:page], :per_page => 4).reverse_order
+      respond_to do |format|
+        format.html
+        format.js
+      end 
+      
     end
 
     
