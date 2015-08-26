b316ba2c0eb646a4f81bd536a9b8bd2b98177610
Jack Clackett <jclackett@gmail.com>
2015-08-26 16:24:07 +0100

    sorting out login funcitonality and styling, still require profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :parents
 
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+
   def parents
     @category_healthy = Category.where(categories: { parent: "healthy" } )
     if @category_healthy.present?
@@ -37,4 +40,21 @@ class ApplicationController < ActionController::Base
     	@category_wise_name = "none"
   	end
   end
+
+   protected
+
+  def configure_devise_permitted_parameters
+    registration_params = [:username, :email, :password, :password_confirmation]
+
+    if params[:action] == 'update'
+      devise_parameter_sanitizer.for(:account_update) { 
+        |u| u.permit(registration_params << :current_password)
+      }
+    elsif params[:action] == 'create'
+      devise_parameter_sanitizer.for(:sign_up) { 
+        |u| u.permit(registration_params) 
+      }
+    end
+  end
+
 end
