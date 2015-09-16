2a2a76f9796c0c7c04191a2521a72cf894c3ddaa
Jack Clackett <jclackett@gmail.com>
2015-09-17 00:05:51 +0100

    sorting logout, FML!!!!!!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,6 +16,13 @@ module ApplicationHelper
 #   content_for(:head) { javascript_include_tag(*args) }
 # end
 
+#gravatar logic
+  def avatar_url(user)
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      return "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
+
    
 end
 
