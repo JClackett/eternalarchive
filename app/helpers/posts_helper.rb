5c98dbf71c7eb982ac986fa3ddae11ef577cb4a0
Jack Clackett <jclackett@gmail.com>
2015-09-19 00:09:32 +0100

    random overlay colors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+
+	def overlay_color
+		color_array = ['red', 'purple', 'white', 'yellow', 'blue', 'green', 'orange']
+		color = color_array.shuffle.first
+		return color
+	end
 end
