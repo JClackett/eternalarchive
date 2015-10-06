2641fa05b29e25fd4712aab03969c81a1cfb1d9e
Jack Clackett <jclackett@gmail.com>
2015-10-06 23:39:40 +0100

    removing source,article url columns, making helper to scrape image from article website and cleaning up

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,4 +5,24 @@ module PostsHelper
 		color = color_array.shuffle.first
 		return color
 	end
+
+	def embed(url)
+	  	if url.include?("youtube")
+	    		youtube_id = url.split("=").last
+	    		content_tag(:iframe, nil, class: "video", src: "http://www.youtube.com/embed/#{youtube_id}?rel=0&amp;modestbranding=1&amp;autohide=1&amp;showinfo=0&amp;controls=1&amp;frameborder=0 autoplay=0" , allowfullscreen:0)
+	    	elsif url.include?("vimeo")
+	    		vimeo_id = url.split("/").last
+	    		content_tag(:iframe, nil, class: "video", src: "https://player.vimeo.com/video/#{vimeo_id}?autoplay=0&color=ff9933&title=0&byline=0&portrait=0", allowfullscreen:0 )
+	    	else	
+	            	article = MetaInspector.new(url, faraday_options: { ssl: { verify: false } })
+	            	article_image = article.images.best
+	    		content_tag(:img, nil, class: "video", src: article_image )
+	    	end
+  	end
+
+	def article_description(url)
+	      		article = MetaInspector.new(url, faraday_options: { ssl: { verify: false } })
+	      		article_title = article.title.split(" — ").first.titleize
+	      		return article_title
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151002225710) do
+ActiveRecord::Schema.define(version: 20151006221605) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "post_id"
@@ -42,10 +42,8 @@ ActiveRecord::Schema.define(version: 20151002225710) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "source"
     t.string   "url"
     t.string   "description"
-    t.string   "article_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
