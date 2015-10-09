03a17f534257afc7227593c2f3061cdec9c2e2f3
Jack Clackett <jclackett@gmail.com>
2015-10-09 23:52:38 +0100

    css and html bull

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,10 @@
 module PostsHelper
 
-	def overlay_color
-		color_array = ['red', 'purple', 'white', 'yellow', 'blue', 'green', 'orange']
-		color = color_array.shuffle.first
-		return color
-	end
+	# def overlay_color
+	# 	color_array = ['red', 'purple', 'white', 'yellow', 'blue', 'green', 'orange']
+	# 	color = color_array.shuffle.first
+	# 	return color
+	# end
 
 	def embed(url)
 	  	if url.include?("youtube")
