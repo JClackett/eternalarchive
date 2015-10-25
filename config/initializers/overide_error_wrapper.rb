3b4948c2172268c37bd1f824a8a5cdd3ddcbc9a4
Jack Clackett <jclackett@gmail.com>
2015-10-25 21:05:53 +0000

    show page, styling, forms shit

--- config/initializers/overide_error_wrapper.rb
+++ config/initializers/overide_error_wrapper.rb
@@ -1,10 +1,10 @@
-# ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-#   class_attr_index = html_tag.index 'class="'
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  class_attr_index = html_tag.index 'class="'
 
-#   if class_attr_index
-#     html_tag.insert class_attr_index+7, 'field-error '
-#   else
-#     html_tag.insert html_tag.index('>'), ' class="field-error"'
-#   end
+  if class_attr_index
+    html_tag.insert class_attr_index+7, 'field-error '
+  else
+    html_tag.insert html_tag.index('>'), ' class="field-error"'
+  end
 
-# end
+end
