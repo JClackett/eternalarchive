173213af79236feb200f46aadca08ddcd8e9a50b
Jack Clackett <jclackett@gmail.com>
2015-10-28 23:42:53 +0000

    removing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,3 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-category_list = [
-  "Fitness",
-  "Nutrition",
-  "Business",
-  "Investing",
-  "Philosophy",
-  "Science",
-  "Culture",
-  "Sociology",
-  "Politics",
-]
-
-category_list.each do |name|
-  Category.create( name: name)
-end
-
-users_list = [
-  { email: 'jclackett@gmail.com', password: 'password', username: 'MacClayzon', admin: true },
-  { email: 'georgeborg00@hotmail.com', password: 'password', username: 'MacBlazin', admin: true },
-  { email: 'tester@gmail.com', password: 'password', username: 'Tester', admin: false },
-  { email: 'dlec2@gmail.com', password: 'password', username: 'MacStump', admin: true },
-]
-
-users_list.each do |user|
-  User.create(user)
-end
