f4f09e3f8ff76d1a91924bacb81c72a9fdc7470b
Jack Clackett <jclackett@gmail.com>
2015-11-03 13:34:54 +0000

    changing category name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -57,6 +57,7 @@ class CategoriesController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
