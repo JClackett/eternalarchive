8aeb6a516b4d93e3225cbaf762f8f9166eeeb9f8
Jack Clackett <jclackett@gmail.com>
2015-11-05 20:47:09 +0000

    adding animations and updates for master

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show, :topvids, :mostrecent, :upvote, :random]
+    before_action :authenticate_user!, except: [:index, :show, :topvids, :mostrecent, :upvote, :shuffle]
 
     
   # GET /posts
