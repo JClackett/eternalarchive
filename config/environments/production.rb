c59042728e9d5af11619544119576fff7e93119d
Jack Clackett <jclackett@gmail.com>
2015-11-07 22:05:09 +0000

    testing the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,7 @@
 Rails.application.configure do
 
    config.action_mailer.default_url_options = {
-    host: 'eternalarchive.co.uk',
-    protocol: 'http'
+    "eternalarchive.co.uk"
   }
 
   # Settings specified here will take precedence over those in config/application.rb.
