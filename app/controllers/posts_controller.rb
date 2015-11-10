333eb3ab32f7216880c554abf43a7be55ed450dd
Jack Clackett <jclackett@gmail.com>
2015-11-10 22:33:04 +0000

    allowing permission to most liked when not a user again!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!, except: [:index, :show, :topvids, :mostrecent, :upvote, :shuffle]
+    before_action :authenticate_user!, except: [:index, :show, :mostliked, :mostrecent, :upvote, :shuffle]
 
     
   # GET /posts
