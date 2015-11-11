af8ecc79a135d5b07b6feb64d1a755b1c37a7e0e
Jack Clackett <jclackett@gmail.com>
2015-11-11 17:23:05 +0000

    adding shuffle catehory functionality and stylin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-class PostsController < ApplicationController
+ class PostsController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
     before_action :authenticate_user!, except: [:index, :show, :mostliked, :mostrecent, :upvote, :shuffle]
 
@@ -93,7 +93,11 @@ class PostsController < ApplicationController
   end
 
   def shuffle
-      @post=Post.all.shuffle.first
+      if params[:p] == "shuffle" 
+        @post=Post.all.shuffle.first
+      else  
+        @post=Post.joins(:categories).where(categories: { name: params[:p] } ).shuffle.first
+      end
       render :show
   end
 
